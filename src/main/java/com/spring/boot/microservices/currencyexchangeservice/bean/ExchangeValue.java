ce13377b73564a4f24a6672cef40b079ee241a6c
Krishna <surekrrishna@gmail.com>
2018-09-17 15:08:51 +0530

    added toString()

--- src/main/java/com/spring/boot/microservices/currencyexchangeservice/bean/ExchangeValue.java
+++ src/main/java/com/spring/boot/microservices/currencyexchangeservice/bean/ExchangeValue.java
@@ -75,5 +75,11 @@ public class ExchangeValue {
 	public void setPort(int port) {
 		this.port = port;
 	}
-						
+
+	@Override
+	public String toString() {
+		return "ExchangeValue [id=" + id + ", from=" + from + ", to=" + to + ", conversionMultiple="
+				+ conversionMultiple + ", port=" + port + "]";
+	}
+								
 }
